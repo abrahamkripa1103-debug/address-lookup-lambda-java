f395c9a739cef4086e5feedee632bf91c20fc7d6
kripa abraham <kripaatcochin@gmail.com>
2025-09-26 23:05:08 +1000

    updated pom.xml and handler

--- src/main/java/com/example/lambda/handler/StreamLambdaHandler.java
+++ src/main/java/com/example/lambda/handler/StreamLambdaHandler.java
@@ -1,8 +1,6 @@
 package com.example.lambda.handler;
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
-import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
-import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
@@ -14,13 +12,14 @@ import java.io.OutputStream;
 
 public class StreamLambdaHandler implements RequestStreamHandler {
 
-    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    // No explicit AwsHttpApiV2Proxy* types here
+    private static final SpringBootLambdaContainerHandler<?, ?> handler;
 
     static {
         try {
-            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
+            // IMPORTANT: use HTTP API v2 adapter for Function URLs
+            handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(Application.class);
         } catch (ContainerInitializationException e) {
-            // If we fail here, we rethrow so the init error shows in CloudWatch logs.
             throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
     }
